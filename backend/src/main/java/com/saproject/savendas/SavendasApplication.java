b77873f34de48bb1ff87ffd0ce31e60a5fea6b76
sealexsandro <sealexsandro@gmail.com>
2021-05-04 09:11:41 -0300

    Project created

---
+++ backend/src/main/java/com/saproject/savendas/SavendasApplication.java
@@ -0,0 +1,13 @@
+package com.saproject.savendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/saproject/savendas/SavendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saproject.savendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
